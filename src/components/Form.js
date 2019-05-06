d7a795a96b311997ffcb5338891b07c8285c52a6
Brad Hain <bradhain@gmail.com>
2019-05-06 10:12:40 -0600

    Added comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,55 +1,84 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Form extends Component {
-
   state = {
-    name: '',
-    address: '',
-    phone: ''
-  }
-
-
-
+    name: "",
+    address: "",
+    phone: ""
+  };
+//This is the fetch that does my Post of create
+  handleSubmit = async e => {
+    e.preventDefault();
+    const data = JSON.stringify(this.state);
+    await fetch("http://localhost:4000", {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  };
 
   render() {
-
-    console.log(this.state);console.log(this);
+    console.log(this.state);
+    console.log(this);
     return (
       <div>
         <h1>Form</h1>
-        <Link to="/"><button type="button" className="btn btn-primary">Home</button></Link>
-        <br/>
-        <br/>
-        <Link to="/people"><button type="button" className="btn btn-primary">People</button></Link>
-
+        <Link to="/">
+          <button type="button" className="btn btn-primary">
+            Home
+          </button>
+        </Link>
+        <br />
+        <br />
+        <Link to="/people">
+          <button type="button" className="btn btn-primary">
+            People
+          </button>
+        </Link>
+        {/* My Form data is sent to my state by using the onChange enent to set the state */}
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="InputName">Name</label>
-            <input type="text" className="form-control" id="InputName" aria-describedby="NameHelp"
-                   placeholder="Name" onChange={e => this.setState({name: e.target.value})}/>
+            <input
+              type="text"
+              className="form-control"
+              id="InputName"
+              aria-describedby="NameHelp"
+              placeholder="Name"
+              onChange={e => this.setState({ name: e.target.value })}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="InputAddress">Address</label>
-            <input type="address" className="form-control" id="InputAddress" placeholder="Address"
-                   onChange={e => this.setState({ address: e.target.value })}/>
+            <input
+              type="address"
+              className="form-control"
+              id="InputAddress"
+              placeholder="Address"
+              onChange={e => this.setState({ address: e.target.value })}
+            />
           </div>
-        <div className="form-group">
-          <label htmlFor="InputPhone">Password</label>
-          <input type="text" className="form-control" id="InputPhone" placeholder="Phone"
-                 onChange={e => this.setState({ phone: e.target.value })}/>
-        </div>
+          <div className="form-group">
+            <label htmlFor="InputPhone">Phone</label>
+            <input
+              type="text"
+              className="form-control"
+              id="InputPhone"
+              placeholder="Phone"
+              onChange={e => this.setState({ phone: e.target.value })}
+            />
+          </div>
 
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-
-
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
-    )
-      ;
+    );
   }
 }
 
 export default Form;
-
--- src/components/People.js
+++ src/components/People.js
@@ -1,66 +1,54 @@
-import React, {Component, Fragment} from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 
 class People extends Component {
-
-
-    state = {
-      people: [],
-      name: '',
-      address: '',
-      password: ''
-    }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-  handleUpdate = async (_id) => {
-    const update = JSON.stringify({ ...this.state })                                                                                                              
-    await fetch('http://localhost:4000/' + _id, {
-      method: 'PUT',
+  state = {
+    people: [],
+    
+  };
+// this is my update
+  handleUpdate = async (e, _id) => {
+    e.preventDefault();
+    const update = JSON.stringify(this.state);
+    await fetch("http://localhost:4000/" + _id, {
+      method: "put",
       body: update,
       headers: {
         "Content-Type": "application/json"
       }
     });
-    await this.getPeople()
+    await this.getPeople();
   };
-
-
-    handleSubmit = async (_id) => {
-      await fetch('http://localhost:4000/' + _id, {
-      method: 'delete'
+// This is my Delete fetch
+  handleSubmit = async _id => {
+    await fetch("http://localhost:4000/" + _id, {
+      method: "DELETE"
     });
-    await this.getPeople()
+    await this.getPeople();
   };
-
-
+// this is my fetch that does a "get"
   getPeople = async () => {
-
-    return fetch('http://localhost:4000')
+    return fetch("http://localhost:4000")
       .then(results => {
         return results.json();
       })
-      .then(data => {
-        this.setState({people: data})
-      })
-  }
-  filterSenators= (e) => {
-    let senators = this.state.people
-    senators = senators.filter(senator => {
-      return senator.name.toLowerCase().search(
-        e.target.value.toLowerCase()) !== -1;
+      .then(results => {
+        this.setState({ people: results });
+      });
+  };
+  filterPeople = e => {
+    let people = this.state.people;
+    people = people.filter(person => {
+      return (
+        person.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+      );
     });
-    this.setState({people: senators});
+    this.setState({ people: people });
+  };
+
+  async componentDidMount() {
+    await this.getPeople();
   }
-
-
-
-  async componentWillMount() {
-
-    await this.getPeople()
-  }
-
-  
-
 
   render() {
     console.log(this);
@@ -69,66 +57,109 @@ class People extends Component {
     return (
       <Fragment>
         <div>
-          <Link to="/"><button type="button" className="btn btn-primary">Home</button></Link>
-        <br/>
-        <form>
-        <fieldset className="form-group">
-        <input type="text" className="form-control form-control-sm" placeholder="Search" onChange={ this.filterSenators}/>
-        </fieldset>
-        </form>
-        <br/>
-        <div>
-            {this.state.people.map
-            ((people,index) =>
+          <Link to="/">
+            <button type="button" className="btn btn-primary">
+              Home
+            </button>
+          </Link>
+          <br />
+          <form>
+            <fieldset className="form-group">
+              <input
+                type="text"
+                className="form-control form-control-sm"
+                placeholder="Search"
+                onChange={this.filterPeople}
+              />
+            </fieldset>
+          </form>
+          <br />
+          <div>
+            {this.state.people.map((people, index) => (
               <div className="border" key={index}>
                 <h3> Name: {people.name}</h3>
                 <p> Phone: {people.phone}</p>
                 <p> Address: {people.address}</p>
-
-                <button type="button" className="btn btn-primary" onClick={() => this.handleSubmit(people._id)}>Delete</button>
-
-
-                <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Update
+                      
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => this.handleSubmit(people._id)}
+                >
+                  Delete
                 </button>
 
-                <div className="modal fade bd-example-modal-sm" tabIndex="-1" role="dialog" aria-labelledby="mySmallModalLabel"
-                     aria-hidden="true">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  data-toggle="modal"
+                  data-target=".bd-example-modal-sm"
+                >
+                  Update
+                </button>
+
+                <div
+                  className="modal fade bd-example-modal-sm"
+                  tabIndex="-1"
+                  role="dialog"
+                  aria-labelledby="mySmallModalLabel"
+                  aria-hidden="true"
+                >
                   <div className="modal-dialog modal-sm">
                     <div className="modal-content">
-
-                      <form onSubmit={e =>this.handleUpdate(people._id)}>
+                      <form onSubmit={e => this.handleUpdate(people._id)}>
                         <div className="form-group">
                           <label htmlFor="InputName">Name</label>
-                          <input type="text" className="form-control" id="InputName" aria-describedby="NameHelp"
-                                 placeholder="Name" onChange={e => this.setState({name: e.target.value})}/>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="InputName"
+                            aria-describedby="NameHelp"
+                            placeholder="Name"
+                            onChange={e =>
+                              this.setState({ name: e.target.value })
+                            }
+                          />
                         </div>
                         <div className="form-group">
                           <label htmlFor="InputAddress">Address</label>
-                          <input type="address" className="form-control" id="InputAddress" placeholder="Address"
-                                 onChange={e => this.setState({ address: e.target.value })}/>
+                          <input
+                            type="address"
+                            className="form-control"
+                            id="InputAddress"
+                            placeholder="Address"
+                            onChange={e =>
+                              this.setState({ address: e.target.value })
+                            }
+                          />
                         </div>
                         <div className="form-group">
-                          <label htmlFor="InputPhone">Password</label>
-                          <input type="text" className="form-control" id="InputPhone" placeholder="Phone"
-                                 onChange={e => this.setState({ phone: e.target.value })}/>
+                          <label htmlFor="InputPhone">Phone</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="InputPhone"
+                            placeholder="Phone"
+                            onChange={e =>
+                              this.setState({ phone: e.target.value })
+                            }
+                          />
                         </div>
 
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary">
+                          Submit
+                        </button>
                       </form>
-
-
                     </div>
                   </div>
                 </div>
-
               </div>
-
-            )}
+            ))}
           </div>
         </div>
       </Fragment>
-    )
+    );
   }
 }
 
-export default People
+export default People;
